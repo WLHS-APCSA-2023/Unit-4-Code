c267a82f1955c4a468cd35396b4336a354ab9284
smithbwlhs <88860661+smithbwlhs@users.noreply.github.com>
2023-11-05 18:25:39 -0800

    Create MoreLoopFun.java

---
+++ MoreLoopFun.java
@@ -0,0 +1,41 @@
+public class MoreLoopFun {
+    public static void main(String[] args) {
+        /*
+        for(initialization; boolean; update){
+            what we want to do
+        }
+         */
+
+        //a for loop that counts by ones from 1 to 5
+        for(int start = 1; start < 5; start++ ){
+            System.out.println(start);
+        }
+
+        //a for loop that prints the powers of two up to 100
+        //can you have an infinite for loop?
+
+
+        //write a for loop that counts by 4s from 5 up to 50
+
+
+        //write a for loop that sums the numbers from 1 to 10
+        int total = 0;
+
+        ////////////////////////////////////////////////////
+        //for loops compared to while loops////////////////
+        //////////////////////////////////////////////////
+
+        for(int num = 1; num <= 5; num++){
+            System.out.println(num);
+        }
+        System.out.println("Finished!");
+
+        int num = 1;
+        while(num <= 5){
+            System.out.println(num);
+            num++;
+        }
+        System.out.println("Finished!");
+
+    }
+}
